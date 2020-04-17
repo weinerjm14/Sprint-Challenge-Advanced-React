3e818130381135e47f8154d5ae9239eef7f3be91
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-04-17 19:35:17 -0400

    recieving data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import axios from "axios";
 import "./App.css";
 
-class  App extends React.Component () {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      initialData: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        this.setState({ initialData: res.data });
+      })
+      .catch(err => {
+        console.log(`error`, err);
+      });
+  }
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
